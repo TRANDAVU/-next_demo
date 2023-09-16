45251d2f6e50d15931052c121f818a9398a4c429
Trần Hoài Dạ Vũ <52185531+TRANDAVU@users.noreply.github.com>
2023-09-16 12:05:46 +0700

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script';
 export default function Document() {
   return (
     <Html lang="en">
@@ -6,21 +7,21 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script async type="text/javascript" src="/js/jquery.min.js"></script>
-        <script async type="text/javascript" src="/js/jquery-migrate-3.0.1.min.js"></script>
-        <script async type="text/javascript" src="/js/popper.min.js"></script>
-        <script async type="text/javascript" src="/js/bootstrap.min.js"></script>
-        <script async type="text/javascript" src="/js/jquery.easing.1.3.js"></script>
-        <script async type="text/javascript" src="/js/jquery.waypoints.min.js"></script>
-        <script async type="text/javascript" src="/js/jquery.stellar.min.js"></script>
-        <script async type="text/javascript" src="/js/owl.carousel.min.js"></script>
-        <script async type="text/javascript" src="/js/jquery.magnific-popup.min.js"></script>
-        <script async type="text/javascript" src="/js/aos.js"></script>
-        <script async type="text/javascript" src="/js/jquery.animateNumber.min.js"></script>
-        <script async type="text/javascript" src="/js/bootstrap-datepicker.js"></script>
-        <script async type="text/javascript" src="/js/jquery.timepicker.min.js"></script>
-        <script async type="text/javascript" src="/js/scrollax.min.js"></script>
-        <script async type="text/javascript" src="/js/main.js"></script>
+        <Script src="/js/jquery.min.js" />
+        <Script src="/js/jquery-migrate-3.0.1.min.js" />
+        <Script src="/js/popper.min.js" />
+        <Script src="/js/bootstrap.min.js" />
+        <Script src="/js/jquery.easing.1.3.js" />
+        <Script src="/js/jquery.waypoints.min.js" />
+        <Script src="/js/jquery.stellar.min.js" />
+        <Script src="/js/owl.carousel.min.js" />
+        <Script src="/js/jquery.magnific-popup.min.js" />
+        <Script src="/js/aos.js" />
+        <Script src="/js/jquery.animateNumber.min.js" />
+        <Script src="/js/bootstrap-datepicker.js" />
+        <Script src="/js/jquery.timepicker.min.js" />
+        <Script src="/js/scrollax.min.js" />
+        <Script src="/js/main.js" />
       </body>
     </Html>
   )
